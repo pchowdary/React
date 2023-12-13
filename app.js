c587066134c764d08512e5574097fa10ec437678
pchowdary <priyankarayala.pec@gmail.com>
2023-12-13 14:54:47 +0530

    elements & comp

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 //jsx is converted into again react.createelement i.e is a JS object converted into html element on render
-const h1 = <h1>pinki</h1>;
-console.log(h1)
+const elem1 =<span>elem1</span>;
+const Comp1 = ()=> <h1> this is a comp1</h1>
+const Comp2 = ()=>{
+    return( <div>
+        <Comp1></Comp1>
+    <h1>this is from compoenent 2</h1>
+    </div>)
+}
+const elem2 =<h1>{elem1}elem2<Comp2/></h1>;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(h1);
+root.render(Comp2());
