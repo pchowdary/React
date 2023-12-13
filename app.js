dbc55bc3221155a739951347676423994bd23e39
pchowdary <priyankarayala.pec@gmail.com>
2023-12-13 14:13:43 +0530

    jsx

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var h1 = React.createElement("div", { id: "first div" },
-    React.createElement(
-        "div", { id: "second div" },
-        React.createElement("h1", { id: "heading" }, "hello world testing parcel1234567"
-        )));
-       console.log(h1); 
-       var root = ReactDOM.createRoot(document.getElementById('root'));
-       root.render(h1);
+//jsx is converted into again react.createelement i.e is a JS object converted into html element on render
+const h1 = <h1>pinki</h1>;
+console.log(h1)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(h1);
