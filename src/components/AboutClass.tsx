53b0153bb3f774d8b12a798432e97c3a9903cd62
pchowdary <priyankarayala.pec@gmail.com>
2024-05-02 16:07:59 +0530

    added class component

--- src/components/AboutClass.tsx
+++ src/components/AboutClass.tsx
@@ -3,9 +3,20 @@ import React,{Component} from "react";
 class AboutClass extends Component{
     constructor(props){
         super(props)
+        this.state={
+            count:0
+        }
     }
     render(){
-        return <div>this is from about class component {this.props?.name}</div>
+        return <div>
+            this is from about class component {this.props?.name}
+            count is - {this.state.count}
+            <p><button onClick={()=>{
+this.setState({
+    count:this.state.count+1
+})
+            }}>increase</button></p>
+            </div>
     }
 }
 export default AboutClass;
